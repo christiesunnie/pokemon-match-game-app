0dae122660fe46cc352f7d3149e97508031f3c08
christiesunnie <christiesunnie@gmail.com>
2021-02-22 11:19:07 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Pokemon Matching Cards Game User stories
+
+1. As an user, you want to type your name in the input to print the message on the page.
+
+2. As the user, you want to click the new game button to start playing the game and start counting down timer (starting from 2 seconds).
+
+3. As the user, when the game starts you want to click and open the card to choose the matched pair of cards.
+
+4. As the author, you want to subtract the number of lives in the game if the user can't open all of the matched pairs of cards or the timer is out.
+
+5. As the author, you want to print the message to tell the user that the timer is out or the game is over or the user wins the game.
+
+6. As the author, you want to close all the opening cards if the user finds the unmatched pair of cards.
+
+7. As the author, you want to reset the timer when the user click the new game button or when the user wins the game.
+
+8. As the author, you want to reset the full number of lives in the game when the user wins or clicks the new game button after the game is over.
+
+9. As the author, you want to clear the pop-up message when the user clicks the new game button.
+
+10. As the author, you want to close all of the opening cards if the user clicks the new game button after the user wins the game.
---
+++ src/script.js
@@ -0,0 +1,133 @@
+$(document).ready(function () {
+  // Declare the global varaiables
+  let playerName, timer;
+  let curLives = 3;
+  let time = 60;
+
+  const $message = $(".message");
+  const $liveNumber = $(".lives-number");
+  const $cardContainer = $(".card-container");
+  const $cards = $(".card");
+  const $images = $("img");
+
+  //////////////////// Functions ///////////////////////////
+  /* Set timer in UI function */
+  const timerUI = function () {
+    const min = String(Math.trunc(time / 60)).padStart(2, 0);
+    const sec = String(time % 60).padStart(2, 0);
+    $(".time").text(`${min}:${sec}`);
+  };
+
+  // Start timer function
+  const startTimer = function () {
+    timer = setInterval(count, 1000);
+    return timer;
+  };
+
+  // Reset timer function
+  const resetTimer = function () {
+    clearInterval(timer);
+    timer = startTimer();
+  };
+
+  /* Reset number of lives function*/
+  const resetLives = function () {
+    curLives = 3;
+    $liveNumber.text(curLives);
+  };
+
+  // Remove class function
+  const removeClasses = function () {
+    $images.removeClass("active");
+    $cards.removeClass("hidden");
+  };
+
+  // Print the timer to UI and counting function
+  const count = function () {
+    timerUI();
+    // When 0 second, stop timer and subtract the current lives of player
+    if (time === 0) {
+      clearInterval(timer);
+      $liveNumber.text(--curLives);
+      $cardContainer.off("click");
+      removeClasses();
+
+      // Print the messages on UI
+      if (curLives < 3) {
+        $message.text("Time out! Try again!");
+      }
+      if (curLives === 0) {
+        $message.text("Game over!");
+      }
+    }
+
+    // Decrease 1s
+    time--;
+  };
+  count();
+
+  ////////////// Events ////////////////////////////////
+  // Print the welcome message with the player name
+  $("form").on("submit", function (e) {
+    e.preventDefault();
+    playerName = $("#name").val().trim();
+    $(".welcome").text(`Welcome, ${playerName}!`);
+    $("#name").val("");
+  });
+
+  // New game button
+  $(".btn-new--game").on("click", function () {
+    $message.text("");
+    removeClasses();
+    time = 60;
+    resetTimer();
+
+    // Reset lives when game over
+    if (curLives === 0) {
+      resetLives();
+    }
+
+    // Open cards event listener
+    $cardContainer.on("click", ".card", function () {
+      const $this = $(this);
+
+      // Add class function
+      const addClasses = function () {
+        $this.addClass("hidden");
+        $this.children().addClass("active");
+      };
+
+      // Add hidden class to open the card
+      if (!$this.hasClass("hidden")) {
+        addClasses();
+      }
+
+      // Map out 2 cards match the src value
+      const $activeImg = $(".active");
+      if ($activeImg.length === 2) {
+        const srcArr = $activeImg
+          .map(function () {
+            return $(this).attr("src");
+          })
+          .get();
+        if (srcArr[0] === srcArr[1]) {
+          $images.removeClass("active");
+        } else {
+          // Set time out to close the card after 2s
+          setTimeout(function () {
+            removeClasses();
+          }, 500);
+        }
+      }
+
+      // Print the message when winning the game
+      if ($(".hidden").length === 16) {
+        clearTimeout(timer);
+        time = 60;
+        timerUI();
+        resetLives();
+        $message.text("Congratulation! You won!");
+      }
+    });
+  });
+});
