621ccc70f484c3603029f061ae18127c53d78880
christiesunnie <christiesunnie@gmail.com>
2021-02-24 21:18:52 -0500

    Modified timer

--- src/script.js
+++ src/script.js
@@ -21,7 +21,7 @@ $(document).ready(function () {
     // Declare the global varaiables
     let playerName, timer;
     let curLives = 3;
-    let time = 60;
+    let time = 90;
 
     const $message = $(".message");
     const $liveNumber = $(".lives-number");
@@ -117,7 +117,7 @@ $(document).ready(function () {
     $(".btn-new--game").on("click", function () {
         $message.text("");
         removeClasses();
-        time = 60;
+        time = 90;
         resetTimer();
         $cardContainer.empty();
 
@@ -165,7 +165,7 @@ $(document).ready(function () {
             // Print the message when winning the game
             if ($(".hidden").length === 16) {
                 clearTimeout(timer);
-                time = 60;
+                time = 90;
                 timerUI();
                 resetLives();
                 $message.text("Congratulation! You won!");
